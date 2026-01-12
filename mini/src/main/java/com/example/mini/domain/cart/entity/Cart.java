e4072bb7a0ce7c7e507470c874f69e6d48db5bba
EomHyeYeong <hy30051@naver.com>
2026-01-12 15:49:33 +0900

    fix: 엔티티 컬렉션 필드에 @Builder.Default 적용

--- mini/src/main/java/com/example/mini/domain/cart/entity/Cart.java
+++ mini/src/main/java/com/example/mini/domain/cart/entity/Cart.java
@@ -14,6 +14,7 @@ import jakarta.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -35,10 +36,10 @@ public class Cart extends BaseEntity {
 	private Member member;
 
 	@ManyToMany(fetch = FetchType.LAZY)
-	@Setter
+	@Setter @Builder.Default
 	private List<Room> roomList = new ArrayList<>();
 
 	@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-	@Setter
+	@Setter @Builder.Default
 	private List<Reservation> reservationList = new ArrayList<>();
 }
